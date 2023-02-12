7de413ef4a8dc67d95bd2e4f8bc31a70f46ad7f8
Florian Bindereif <Florian.bindereif@gmail.com>
2023-02-12 22:59:46 +0100

    added: erase function to rbt, still untested

--- mine/red_black_tree.hpp
+++ mine/red_black_tree.hpp
@@ -45,11 +45,6 @@ namespace ft
 		
 		private:
 		public:
-
-			// node_pointer&			end_node_() {return root_;}
-			// const_node_pointer		end_node_() const {return root_;}
-			node_pointer&				root_node_() {return root_;}
-			const_node_pointer			root_node_() const {return root_;}
 			
 			node_pointer create_new_node_(const value_type& value, COLOUR colour, node_pointer parent)
 			{
@@ -71,12 +66,36 @@ namespace ft
 			alloc_value_(other.alloc_value_),
 			node_count_(other.node_count_)
 			{
-				//
+				root_ = create_new_node_(value_type(), BLACK);
+				*this = other;
 			}
 
 			red_black_tree(const value_compare& compare, const allocator_type& alloc)
 			:compare_(compare), alloc_node_(alloc), alloc_value_(alloc), node_count_(0)
-			{ left_most_ = root_ = nil_ = create_new_node_(value_type(), BLACK, nil_);}
+			{ 
+				left_most_ = root_ = nil_ = create_new_node_(value_type(), BLACK, nil_);
+			}
+
+			red_black_tree& operator=(const red_black_tree& other)
+			{
+				if (this != &other)
+				{
+					clear();
+					alloc_node_ = other.alloc_node_;
+					alloc_value_ = other.alloc_value_;
+					root_ = tree_copy(other.root_, nil_, other.nil_);
+					node_count_ = other.node_count_;
+					left_most_ = rbt_leftmost(root_);
+				}
+				return *this;
+			}
+
+			~red_black_tree()
+			{
+				clear();
+				alloc_node.destroy(nil_);
+				alloc_node.deallocate(nil_);
+			}
 
 			/***********************************************
 				Iterators
@@ -99,6 +118,18 @@ namespace ft
 			size_type max_size() const { return alloc_node_.max_size(); }
 
 			bool empty() const { return node_count_ == 0 ;}
+		
+		private:
+
+			tree_copy(node_pointer node, node_pointer parent, node_pointer nil)
+			{
+				if (node == nil)
+					return nil_;
+				node_pointer new_node = create_new_node_(node->value, node->colour, parent);
+				new_node->left = tree_copy(node->left, new_node, nil);
+				new_node->left = tree_copy(node->right, new_node, nil);
+				return new_node;
+			}
 
 			void rotate_right_(node_pointer x)
 			{
@@ -133,8 +164,90 @@ namespace ft
 				y->left = x;						//put x on y's left
 				x->parent = y;
 			}
-	
-			void balance_insert(node_pointer node)
+
+			transplant_(node_pointer x, node_pointer y)
+			{
+				if (x->parent == nil_)
+					root_ = y;
+				else if (x == x->parent->left) //x is left child
+					x->parent->left = y;
+				else
+					x->parent->right = y; //x is right child
+				y->parent = x->parent;
+			}
+
+			void balance_erase_(node_pointer node)
+			{
+				node_pointer sibling;
+				while(node != _root && node->coolour == BLACK)
+				{
+					if (node == node->parent->left)
+					{
+						sibling = node->parent->right;
+						if (sibling->colour == RED)
+						{
+							sibling->colour = BLACK;
+							node->parent->colour == RED;
+							rotate_left_(node->parent);
+							sibling = node->parent->right;
+						}
+						if (sibling->left->colour == BLACK && sibling->right->colour == BLACK)
+						{
+							sibling->colour = RED;
+							node = node->parent;
+						}
+						else
+						{
+							if (sibling->right->colour == BLACK)
+							{
+								sibling->left->colour = BLACK;
+								sibling->colour = RED;
+								rotate_right_(sibling);
+								sibling = node->parent->right;
+							}
+							sibling->colour = node->parent->colour;
+							node->parent->colour = BLACK;
+							sibling->right->colour = BLACK;
+							rotate_left_(node->parent);
+							node = root_;
+						}
+					}
+					else
+					{
+						sibling = node->parent->left;
+						if (sibling->colour == RED)
+						{
+							sibling->colour = BLACK;
+							node->parent->colour == RED;
+							rotate_right_(node->parent);
+							sibling = node->parent->left;
+						}
+						if (sibling->right->colour == BLACK && sibling->right->colour == BLACK)
+						{
+							sibling->colour = RED;
+							node = node->parent;
+						}
+						else
+						{
+							if (sibling->left->colour == BLACK)
+							{
+								sibling->right->colour = BLACK;
+								sibling->colour = RED;
+								rotate_left_(sibling);
+								sibling = node->parent->left;
+							}
+							sibling->colour = node->parent->colour;
+							node->parent->colour = BLACK;
+							sibling->left->colour = BLACK;
+							rotate_right_(node->parent);
+							node = root_;
+						}
+					}
+				}
+				node->colour = BLACK;
+			}
+
+			void balance_insert_(node_pointer node)
 			{
 				while(node != root_ && node->parent->colour == RED)
 				{
@@ -185,7 +298,76 @@ namespace ft
 					root_->colour = BLACK;
 				}
 			}
-			
+		
+		public:
+
+			clear()
+			{
+				clear_tree(root_);
+				node_count_ = 0;
+				root_ = nil_;
+				left_most_ = root_;
+			}
+
+			void destroy_node_(node_pointer node)
+			{
+				alloc_value_.destroy(&node->value);
+				alloc_node_.desotroy(node, 1);
+				node = nil_;
+			}
+
+			void clear_tree(node_pointer node)
+			{
+				if (node == nil_ || node == mynullptr) // node == node->_left || node == node->_right
+					return ;
+				clear_tree(node->right);
+				clear_tree(node->left)
+				destroy_node_(node);
+			}
+
+			size_type	erase(iterator pos)
+			{
+				if (pos == end())
+					return 0;
+				node_pointer z = pos.base();
+				node_pointer y = z;
+				node_pointer x;
+				COLOUR original_colour = y->colour;
+
+				if (z->left == nil_)
+				{
+					x = z->right;
+					transplant_(z, x);
+				}
+				else if (z->right == nil_)
+				{
+					x = z->left;
+					transplant_(z, x);
+				}
+				else
+				{
+					y = rbt_leftmost(z->right);
+					original_colour = y->colour;
+					x = y->right;
+					if (y->parent == z)
+						x->parent = y;
+					else
+					{
+						transplant_(y, y->right);
+						y->right = z->right;
+						y->right->parent = y;
+					}
+					transplant_(z, y);
+					y->left = z->left;
+					y->left->parent = y;
+					y->colour = z->colour;
+				}
+				if (original_colour == BLACK)
+					balance_erase_();
+				--node_count_;
+				return 1;
+			}
+
 			ft::pair<iterator, bool> insert(const value_type& value)
 			{
 				node_pointer parent = root_, new_node;
@@ -213,44 +395,46 @@ namespace ft
 				// new_node->nil = new_node->left;
 				if (left_most_->left != nil_)
 					left_most_ = left_most_->left;
-				balance_insert(new_node);
+				balance_insert_(new_node);
 				++node_count_;
 				if (new_node == rbt_rightmost(root_)) //vielleicht nciht notwendig
 					nil_->parent = new_node;
 				return ft::make_pair(iterator(new_node), true);
 			}
 
-			// template <typename Key>
-			// iterator find (const Key& key)
-			// {
-			// 	node_pointer search = root_node_();
-			// 	while(search != mynullptr)
-			// 	{
-			// 		if (compare_(search->value, key))
-			// 			search = search_->right_;
-			// 		else if (compare_(key, search->value))
-			// 			search = search->left_;
-			// 		else
-			// 			return iterator(search);
-			// 	}
-			// 	return (end());
-			// }
+			template <typename Key>
+			iterator find (const Key& key)
+			{
+				node_pointer iter = root_;
+				while(iter != nil_)
+				{
+					if (compare_(iter->value, key))
+						iter = iter_->right_;
+					else if (compare_(key, iter->value))
+						iter = iter->left_;
+					else
+						return iterator(iter);
+				}
+				return (end());
+			}
 
-			// template <typename Key>
-			// const_iterator find (const Key& key) const
-			// {
-			// 	node_pointer search = root_node_();
-			// 	while(search != mynullptr)
-			// 	{
-			// 		if (compare_(search->value, key))
-			// 			search = search_->right_;
-			// 		else if (compare_(key, search->value))
-			// 			search = search->left_;
-			// 		else
-			// 			return const_iterator(search);
-			// 	}
-			// 	return (end());
-			// }
+			template <typename Key>
+			iterator find (const Key& key) const
+			{
+				node_pointer iter = root_;
+				while(iter != nil_)
+				{
+					if (compare_(iter->value, key))
+						iter = iter_->right_;
+					else if (compare_(key, iter->value))
+						iter = iter->left_;
+					else
+						return const_iterator(iter);
+				}
+				return (end());
+			}
+
+			size_type size() {return node_count_};
 
 			void print_from_node_(node_pointer node, const std::string& prefix = "", bool is_left = false,
 			bool is_first = true)
